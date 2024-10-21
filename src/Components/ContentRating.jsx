be7c838485f1a6b8c60ed3a4c09b3d768ac7a301
jboente <jboente90@gmail.com>
2024-10-20 22:44:26 -0400

    added buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -6,10 +6,20 @@ class ContentRating extends Component {
   constructor() {
     super();
   }
+  this.state = {
+    likes: 0,
+    dislikes: 0
+  }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+        <p>Do you like dogs?</p>
+        <div className='rating-buttons'>
+            <button className='like-button'>I love dogs <3!! ( { this.state.likes } )</button>
+            <button className='dislike-button'>Ugh... prefer cats ( { this.state.dislikes } ) </button>
+        </div>
+     </div>
      </>
     );
   }
